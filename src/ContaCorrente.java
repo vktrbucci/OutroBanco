7d205b5e799d407a49110188217906df6c138731
Victor Bucci <vr.bucci@gmail.com>
2021-12-02 19:49:09 -0300

    Modificadores de acesso alterados e criados métodos getters e setters.

---
+++ src/ContaCorrente.java
@@ -0,0 +1,35 @@
+public class ContaCorrente {
+
+    // Por padrão, atributos são sempre private
+    private String agencia;
+    private String numero;
+    private double saldo;
+
+    // Faz sentido manter esse métodos como public, pois formam a interface com o usuário.
+    public void sacar() {
+
+    }
+
+    public void depositar() {
+
+    }
+
+    public void transferir() {
+
+    }
+
+    // Não faz sentido criar métodos setters para nossos atributos, então teremos apenas os getters.
+
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
